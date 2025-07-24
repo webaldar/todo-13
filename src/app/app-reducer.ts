894f08b12c5ddb84328e1cbd0501f1550a00a54f
webaldar <webaldar@gmail.com>
2025-07-24 14:58:35 +0300

    add app slice

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,15 +1,33 @@
-import { createAction, createReducer } from "@reduxjs/toolkit"
+import {createSlice} from "@reduxjs/toolkit"
 
-export const changeThemeModeAC = createAction<{ themeMode: ThemeMode }>("app/changeThemeMode")
 
-const initialState = {
-  themeMode: "light" as ThemeMode,
-}
-
-export const appReducer = createReducer(initialState, (builder) => {
-  builder.addCase(changeThemeModeAC, (state, action) => {
-    state.themeMode = action.payload.themeMode
-  })
+export const appSlice = createSlice({
+  name: 'app',
+  initialState: {
+    themeMode: "dark" as ThemeMode,
+  },
+  reducers: (create)=> {
+    return {
+      changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
+        state.themeMode = action.payload.themeMode
+      })
+    }
+  }
 })
 
+export const appReducer = appSlice.reducer
+export const {changeThemeModeAC} = appSlice.actions
+
 export type ThemeMode = "dark" | "light"
+//
+// export const changeThemeModeAC = createAction<{ themeMode: ThemeMode }>("app/changeThemeMode")
+//
+// const initialState = {
+//   themeMode: "light" as ThemeMode,
+// }
+//
+// export const appReducer = createReducer(initialState, (builder) => {
+//   builder.addCase(changeThemeModeAC, (state, action) => {
+//     state.themeMode = action.payload.themeMode
+//   })
+// })
